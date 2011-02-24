1ef2fc6a6cf050836a863b06a72245dbe3d7fd2f
Michael <MikeyMucaQuoo@michael-lowes-macbook-air.local>
2011-02-24 14:33:40 -0700

    Testing an upload.

---
+++ try_glasses_1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryGlasses1::Application.config.secret_token = '99971a634573b1d46ba0842547ecd999cb08d2adf73da2d64fa095c16a376d43900f9fe9696ae3513705b19f2900a47ba540e6634be96a48f7d2cc3643f3bfb7'
