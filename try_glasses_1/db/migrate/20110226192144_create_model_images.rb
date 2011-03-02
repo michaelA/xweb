0a9d1b41af4c89a7574bd822b8f825056d36b9ed
Michael <MikeyMucaQuoo@michael-lowes-macbook-air.local>
2011-03-02 16:00:26 -0700

    Updated table validation

--- try_glasses_1/db/migrate/20110226192144_create_model_images.rb
+++ try_glasses_1/db/migrate/20110226192144_create_model_images.rb
@@ -1,14 +1,20 @@
 class CreateModelImages < ActiveRecord::Migration
   def self.up
     create_table :model_images do |t|
-      t.string :image_url
-      t.string :gender
+      t.string :image_url,  :null => false
+      t.string :gender,     :null => false
 
       t.timestamps
     end
+    
+    add_index :model_images, :image_url
+    add_index :model_images, :gender
   end
 
   def self.down
+    remove_index :model_images, :image_url
+    remove_index :model_images, :gender
+    
     drop_table :model_images
   end
 end
--- try_glasses_1/db/migrate/20110226192619_create_eye_glasses.rb
+++ try_glasses_1/db/migrate/20110226192619_create_eye_glasses.rb
@@ -1,19 +1,28 @@
 class CreateEyeGlasses < ActiveRecord::Migration
   def self.up
     create_table :eye_glasses do |t|
-      t.string :name
-      t.string :material
-      t.string :color
-      t.string :shape
-      t.decimal :price
+      t.string :name,     :null => false
+      t.string :material, :null => false
+      t.string :color,    :null => false 
+      t.string :shape,    :null => false
+      t.decimal :price,   :null => false, precision => 6, :scale => 2
       t.string :who_can_wear
       t.string :image_url
 
       t.timestamps
     end
+    
+    add_index :eye_glasses, :name
+    add_index :eye_glasses, :who_can_wear
+    add_index :eye_glasses, :image_url
+    
   end
 
   def self.down
+    remove_index :eye_glasses, :name
+    remove_index :eye_glasses, :who_can_wear
+    remove_index :eye_glasses, :image_url
+
     drop_table :eye_glasses
   end
 end
